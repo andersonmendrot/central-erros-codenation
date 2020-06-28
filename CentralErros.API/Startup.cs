177ead339d47edfefb5835f9696e1dc99a1be610
andersonmendrot <anderson.mendrot@gmail.com>
2020-06-27 22:32:24 -0300

    Renomeacão de Data para Infrastructure e Models para Domain

--- CentralErros.API/Startup.cs
+++ CentralErros.API/Startup.cs
@@ -1,12 +1,16 @@
-using CentralErros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace CourseLibrary.API
+namespace CentralErros.API
 {
     public class Startup
     {
@@ -21,12 +25,6 @@ namespace CourseLibrary.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddDbContext<CentralErrosContext>(options =>
-            {
-                options.UseSqlServer(
-                    @"Server = (localdb)\mssqllocaldb; Database = CentralErrosDB; Trusted_Connection = True;");
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,5 +46,3 @@ namespace CourseLibrary.API
         }
     }
 }
-
-
