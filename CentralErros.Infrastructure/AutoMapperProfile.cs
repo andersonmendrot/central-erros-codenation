a81826b666820ef959976f54d36e397d7eecb559
andersonmendrot <anderson.mendrot@gmail.com>
2020-07-23 11:50:24 -0300

    Criação de DTO para remoção de level (inclui LevelId) e inclusão dele no Mapper

--- CentralErros.Infrastructure/AutoMapperProfile.cs
+++ CentralErros.Infrastructure/AutoMapperProfile.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoMapper;
+﻿using AutoMapper;
 using CentralErros.Domain.Models;
 using CentralErros.Infrastructure.DTOs;
 using Environment = CentralErros.Domain.Models.Environment;
@@ -18,6 +15,8 @@ namespace CentralErros.Infrastructure
             CreateMap<Language, LanguageDTO>().ReverseMap();
             CreateMap<Level, LevelDTO>().ReverseMap();
             CreateMap<User, UserDTO>().ReverseMap();
+
+            CreateMap<Level, LevelForDeleteDTO>().ReverseMap();
         }
     }
 }
