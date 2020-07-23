7b5cecebe33255011c7b658b3f710589b857d16e
andersonmendrot <anderson.mendrot@gmail.com>
2020-07-23 12:13:14 -0300

    Desenvolvimento parcial de controller de Level

---
+++ CentralErros.API/Controllers/LevelController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CentralErros.Domain.Models;
+using CentralErros.Domain.Repositories;
+using CentralErros.Infrastructure.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using CentralErros.Infrastructure.Repositories;
+using System.Linq;
+
+namespace CentralErros.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LevelController : ControllerBase
+    {
+        private readonly ILevelRepository _levelRepository;
+        private readonly IMapper _mapper;
+        public LevelController(ILevelRepository levelRepository, IMapper mapper)
+        {
+            _levelRepository = levelRepository;
+            _mapper = mapper; 
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<LevelDTO>> GetLevels()
+        {
+            var service = _levelRepository.GetAll();
+            return Ok(_mapper.Map<IEnumerable<LevelDTO>>(service));
+        }
+
+        [HttpPost]
+        public ActionResult<LevelDTO> CreateLevel(LevelDTO level)
+        {
+            if (String.IsNullOrWhiteSpace(level.Name))
+            {
+                return StatusCode(400);
+            }
+            var entity = _mapper.Map<Level>(level);
+            _levelRepository.Save(entity);
+
+            var levelDto = _mapper.Map<LevelDTO>(_levelRepository.GetById(entity.Id));
+            
+            return Ok(_mapper.Map<LevelDTO>(levelDto));
+        }
+
+        [HttpDelete]
+        public ActionResult<LevelDTO> RemoveLevel(int levelId)
+        {
+            //se houver algum registro de erro com o level que se deseja remover
+            //entao deve-se retornar um codigo de erro
+            if (_levelRepository.HasErrorsWithLevelId(levelId))
+            {
+                return StatusCode(404); 
+            }
+
+            if(_levelRepository.GetById(levelId) == null)
+            {
+                return StatusCode(404);
+            }
+
+            return Ok(_mapper.Map<ErrorDTO>(_levelRepository.Remove(levelId)));
+        }
+    }
+}
